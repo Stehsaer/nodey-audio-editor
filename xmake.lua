769f1abb3c0527d3a4cfdd60666de695a601eb32
Stehsaer <stehsaer@outlook.com>
2025-06-09 21:36:33 +0800

    锁定库的版本号，统一使用xrepo库

--- xmake.lua
+++ xmake.lua
@@ -1,11 +1,11 @@
 add_rules("mode.debug", "mode.release")
 
-add_requires("imgui", {alias="imgui", configs={sdl2=true, sdl2_renderer=true, wchar32=true}})
-add_requires("jsoncpp")
-add_requires("libsdl2",{configs={shared=true, sdl2_image=false, sdl2_mixer=true, sdl2_ttf=false}, system=false})
-add_requires("ffmpeg", {configs={shared=true, ffmpeg=false}})
-add_requires("boost", {configs={cmake=false, fiber=true}})
-add_requires("nativefiledialog")
+add_requires("imgui v1.91.9",			{system=false, configs={sdl2=true, sdl2_renderer=true, wchar32=true}})
+add_requires("jsoncpp 1.9.6",			{system=false})
+add_requires("libsdl2 2.32.2",			{system=false, configs={shared=true, sdl2_image=false, sdl2_mixer=true, sdl2_ttf=false}})
+add_requires("ffmpeg 7.1",				{system=false, configs={shared=true, ffmpeg=false}})
+add_requires("boost 1.88.0",			{system=false, configs={cmake=false, fiber=true}})
+add_requires("nativefiledialog 1.1.6",	{system=false})
 
 includes("third-party")
 
